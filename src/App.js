0f4d844c55db13a00624578c292318b823339bee
Andrew Koller <andykoller55@yahoo.com>
2024-04-04 15:29:58 -0400

    adding in a navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Andrew's Learning React Webpage!</h1>
+      <Navbar />
+      <div className='content'>
+        <h1>Welcome to Andrew's Learning React Webpage!</h1>
+      </div>
+      
     </div>
   );
 }
