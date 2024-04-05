5703441b9ac1d1bf934817ce38c15d396c842e1a
Andrew Koller <andykoller55@yahoo.com>
2024-04-04 22:11:27 -0400

    added click me button

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("hello user");
+    }
+
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
         </div>
       );
 }
