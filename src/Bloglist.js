308e83b190fc77322906a9bc2273ad610883dce3
Andrew Koller <andykoller55@yahoo.com>
2024-04-09 21:11:01 -0400

    added more blog functionality using useState and useEffect

---
+++ src/Bloglist.js
@@ -0,0 +1,20 @@
+const Bloglist = ({blogs, title, handleDelete}) => {
+  // const blogs = props.blogs;
+
+  return (  
+      <div className="bloglist">
+        <h1>{title}</h1>
+        {blogs.map((blog) => (
+        <div className='blog-preview' key={blog.id}>
+          <h2>{blog.title}</h2>
+          <h4>{blog.body}</h4>
+          <p>Written by {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
+        </div>
+    ))}
+      </div>
+  );
+}
+
+
+export default Bloglist;
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,32 @@
+import { useState, useEffect } from "react";
+import Bloglist from './Bloglist';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {title: 'My new website', body: 'This is going to be my developer portfolio website!', author: 'Andrew Koller', id: 1},
+    {title: '100 Day of Coding', body: "This will be a list of what I've accomplished each day", author: 'Andrew Koller', id: 2},
+    {title: 'Focus on React', body: 'My focus will be on mastering React and getting a front end dev job', author: 'Emily Osburn', id: 3}
+  ]);
+  
+  const [name, setName] = useState('mario');
 
-    const handleClick = () => {
-        console.log("hello user");
-    }
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(name);
+  }, [name]);
 
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-        </div>
-      );
+  return (
+    <div>
+     <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+     <button onClick={() => setName('Luigi')}>change name</button>
+     <p>{ name }</p>
+    </div>
+  )
 }
- 
+
 export default Home;
