82abfb1b42b4dcbf6f4695e54ad0619d5fc3185d
Andrew Koller <andykoller55@yahoo.com>
2024-04-12 23:20:50 -0400

    added the rest of the days. updated the navbar to exclude home and article until i create these

--- src/Home.js
+++ src/Home.js
@@ -373,101 +373,246 @@ const Home = () => {
       body: ["Working through the Pokemon Search App Project"], 
       id: 60
     },
-    
-    // {title: 'Day 7', body: "", id: 7},
-    // {title: 'Day 8', body: "", id: 8},
-    // {title: 'Day 9', body: "", id: 9},
-    // {title: 'Day 10', body: "", id: 10},
-    // {title: 'Day 11', body: "", id: 11},
-    // {title: 'Day 12', body: "", id: 12},
-    // {title: 'Day 13', body: "", id: 13},
-    // {title: 'Day 14', body: "", id: 14},
-    // {title: 'Day 15', body: "", id: 15},
-    // {title: 'Day 16', body: "", id: 16},
-    // {title: 'Day 17', body: "", id: 17},
-    // {title: 'Day 18', body: "", id: 18},
-    // {title: 'Day 19', body: "", id: 19},
-    // {title: 'Day 20', body: "", id: 20},
-    // {title: 'Day 21', body: "", id: 21},
-    // {title: 'Day 22', body: "", id: 22},
-    // {title: 'Day 23', body: "", id: 23},
-    // {title: 'Day 24', body: "", id: 24},
-    // {title: 'Day 25', body: "", id: 25},
-    // {title: 'Day 26', body: "", id: 26},
-    // {title: 'Day 27', body: "", id: 27},
-    // {title: 'Day 28', body: "", id: 28},
-    // {title: 'Day 29', body: "", id: 29},
-    // {title: 'Day 30', body: "", id: 30},
-    // {title: 'Day 31', body: "", id: 31},
-    // {title: 'Day 32', body: "", id: 32},
-    // {title: 'Day 33', body: "", id: 33},
-    // {title: 'Day 34', body: "", id: 34},
-    // {title: 'Day 35', body: "", id: 35},
-    // {title: 'Day 36', body: "", id: 36},
-    // {title: 'Day 37', body: "", id: 37},
-    // {title: 'Day 38', body: "", id: 38},
-    // {title: 'Day 39', body: "", id: 39},
-    // {title: 'Day 40', body: "", id: 40},
-    // {title: 'Day 41', body: "", id: 41},
-    // {title: 'Day 42', body: "", id: 42},
-    // {title: 'Day 43', body: "", id: 43},
-    // {title: 'Day 44', body: "", id: 44},
-    // {title: 'Day 45', body: "", id: 45},
-    // {title: 'Day 46', body: "", id: 46},
-    // {title: 'Day 47', body: "", id: 47},
-    // {title: 'Day 48', body: "", id: 48},
-    // {title: 'Day 49', body: "", id: 49},
-    // {title: 'Day 50', body: "", id: 50},
-    // {title: 'Day 51', body: "", id: 51},
-    // {title: 'Day 52', body: "", id: 52},
-    // {title: 'Day 53', body: "", id: 53},
-    // {title: 'Day 54', body: "", id: 54},
-    // {title: 'Day 55', body: "", id: 55},
-    // {title: 'Day 56', body: "", id: 56},
-    // {title: 'Day 57', body: "", id: 57},
-    // {title: 'Day 58', body: "", id: 58},
-    // {title: 'Day 59', body: "", id: 59},
-    // {title: 'Day 60', body: "", id: 60},
-    // {title: 'Day 61', body: "", id: 61},
-    // {title: 'Day 62', body: "", id: 62},
-    // {title: 'Day 63', body: "", id: 63},
-    // {title: 'Day 64', body: "", id: 64},
-    // {title: 'Day 65', body: "", id: 65},
-    // {title: 'Day 66', body: "", id: 66},
-    // {title: 'Day 67', body: "", id: 67},
-    // {title: 'Day 68', body: "", id: 68},
-    // {title: 'Day 69', body: "", id: 69},
-    // {title: 'Day 70', body: "", id: 70},
-    // {title: 'Day 71', body: "", id: 71},
-    // {title: 'Day 72', body: "", id: 72},
-    // {title: 'Day 73', body: "", id: 73},
-    // {title: 'Day 74', body: "", id: 74},
-    // {title: 'Day 75', body: "", id: 75},
-    // {title: 'Day 76', body: "", id: 76},
-    // {title: 'Day 77', body: "", id: 77},
-    // {title: 'Day 78', body: "", id: 78},
-    // {title: 'Day 79', body: "", id: 79},
-    // {title: 'Day 80', body: "", id: 80},
-    // {title: 'Day 81', body: "", id: 81},
-    // {title: 'Day 82', body: "", id: 82},
-    // {title: 'Day 83', body: "", id: 83},
-    // {title: 'Day 84', body: "", id: 84},
-    // {title: 'Day 85', body: "", id: 85},
-    // {title: 'Day 86', body: "", id: 86},
-    // {title: 'Day 87', body: "", id: 87},
-    // {title: 'Day 88', body: "", id: 88},
-    // {title: 'Day 89', body: "", id: 89},
-    // {title: 'Day 90', body: "", id: 90},
-    // {title: 'Day 91', body: "", id: 91},
-    // {title: 'Day 92', body: "", id: 92},
-    // {title: 'Day 93', body: "", id: 93},
-    // {title: 'Day 94', body: "", id: 94},
-    // {title: 'Day 95', body: "", id: 95},
-    // {title: 'Day 96', body: "", id: 96},
-    // {title: 'Day 97', body: "", id: 97},
-    // {title: 'Day 98', body: "", id: 98},
-    // {title: 'Day 99', body: "", id: 99},
-    // {title: 'Day 100', body: "", id: 100}
+    {
+      title: 'Day 61',
+      body: ["Finished the Pokemon Search App Project and successfully completed the JavaScript Algorithms and Data Structures Certification!",
+            "Starting Front End Development Libraries - Bootstrap"], 
+      id: 61
+    },
+    {
+      title: 'Day 62',
+      body: ["Finished Front End Development Libraries - Bootstrap"], 
+      id: 62
+    },
+    {
+      title: 'Day 63',
+      body: ["Front End Libraries - working on jQuery"], 
+      id: 63
+    },
+    {
+      title: 'Day 64',
+      body: ["Completed - Front End Libraries - jQuery",
+            "Starting - Front End Libraries - Saas"], 
+      id: 64
+    },
+    {
+      title: 'Day 65',
+      body: ["Finished - Front End Libraries - Saas",
+            "Starting - Front End Libraries - React!!!"], 
+      id: 65
+    },
+    {
+      title: 'Day 66',
+      body: ["Working through - Front End Libraries - React"], 
+      id: 66
+    },
+    {
+      title: 'Day 67',
+      body: ["Working through - Front End Libraries - React"], 
+      id: 67
+    },
+    {
+      title: 'Day 68',
+      body: ["Working through - Front End Libraries - React"], 
+      id: 68
+    },
+    {
+      title: 'Day 69',
+      body: ["Work in Progress - Front End Libraries - React 62% Completed"], 
+      id: 69
+    },
+    {
+      title: 'Day 70',
+      body: ["Work in Progress - Front End Libraries - React 70% Completed"], 
+      id: 70
+    },
+    {
+      title: 'Day 71',
+      body: ["Work in Progress - Front End Libraries - React 85% Completed"], 
+      id: 71
+    },
+    {
+      title: 'Day 72',
+      body: ["Work in Progress - Front End Libraries - React 87% Completed"], 
+      id: 72
+    },
+    {
+      title: 'Day 73',
+      body: ["Work in Progress - Front End Libraries - React 89% Completed"], 
+      id: 73
+    },
+    {
+      title: 'Day 74',
+      body: ["Work in Progress - Front End Libraries - React 98% Completed"], 
+      id: 74
+    },
+    {
+      title: 'Day 75',
+      body: ["COMPLETED - Front End Libraries - React!!!",
+            "Starting - Front End Libraries - Redux 29% Completed"], 
+      id: 75
+    },
+    {
+      title: 'Day 76',
+      body: ["Work in Progress - Front End Libraries - Redux 47% Completed"], 
+      id: 76
+    },
+    {
+      title: 'Day 77',
+      body: ["Work in Progress - Front End Libraries - Redux 71% Completed"], 
+      id: 77
+    },
+    {
+      title: 'Day 78',
+      body: ["COMPLETED - Front End Libraries - Redux!"], 
+      id: 78
+    },
+    {
+      title: 'Day 79',
+      body: ["Work in Progress - Front End Libraries - Redux and React 10% Completed"], 
+      id: 79
+    },
+    {
+      title: 'Day 80',
+      body: ["Work in Progress - Front End Libraries - Redux and React 10% Completed"], 
+      id: 80
+    },
+    {
+      title: 'Day 81',
+      body: ["Work in Progress - Front End Libraries - Redux and React 60% Completed"], 
+      id: 81
+    },
+    {
+      title: 'Day 82',
+      body: ["Completed - Front End Libraries - Redux and React"], 
+      id: 82
+    },
+    {
+      title: 'Day 83',
+      body: ["Work in Progress - Random Quote Generator",
+            "Got my vs code environment up and running and some basic structure set up. Got the cdn testing script in there and working fine. Already passing 4 tests but still lots of work  to be done. Super excited to be writing in vs code independently and building something using React"], 
+      id: 83
+    },
+    {
+      title: 'Day 84',
+      body: ["Work in Progress - Random Quote Generator",
+            "Some pains getting more familiar with vscode"], 
+      id: 84
+    },
+    {
+      title: 'Day 85',
+      body: ["Work in Progress - Random Quote Generator",
+            "Redid things using React now. Took me a bit to get the testing environment up and running using React but I got there. Now im moving along - 9/12 tests complete. Will plan to get most of it done tomorrow and take an extra day to make it clean. Really excited using React now"], 
+      id: 85
+    },
+    {
+      title: 'Day 86',
+      body: ["Work in Progress - Random Quote Generator",
+            "Trying to figure out how to fetch data from an API using react. Ran into some issues. Will try more tomorrow"], 
+      id: 86
+    },
+    {
+      title: 'Day 87',
+      body: ["Work in Progress - Random Quote Generator",
+            "Managed to work for an hour. Still struggling to fetch data from api using react. Proving to be a real struggle. Will keep trying tomorrow"], 
+      id: 87
+    },
+    {
+      title: 'Day 88',
+      body: ["Work in Progress - Random Quote Generator",
+            "Figured out how to fetch data from api using react so I can now display multiple quotes from a public api. Need to figure out how to randomly show 1 now",
+            "Watched a video on youTube about fetching data from the pokemon api - this is review but it was good to see this again. ill finish this project eventually"], 
+      id: 88
+    },
+    {
+      title: 'Day 89',
+      body: ["Work in Progress - Random Quote Generator",
+            "10/12 tests passing. ALMOST THERE!!! I'm hoping tomorrow is the day :)"], 
+      id: 89
+    },
+    {
+      title: 'Day 90',
+      body: ["Work in Progress - Random Quote Generator",
+            "Still workin on it"], 
+      id: 90
+    },
+    {
+      title: 'Day 91',
+      body: ["Completed - Random Quote Generator",
+            "All test passing"], 
+      id: 91
+    },
+    {
+      title: 'Day 92',
+      body: ["Focusing on fundamentals of React. Watching a Udemy course and course on youtube",
+            "Spend a couple hours on the Udemy course today",
+            "Plan on doing the same tomorrow and then starting next project later this week"], 
+      id: 92
+    },
+    {
+      title: 'Day 93',
+      body: ["Watched youTube video on React dynamic variables and multiple components",
+            "Put the code for the random quote generator on my GitHub"], 
+      id: 93
+    },
+    {
+      title: 'Day 94',
+      body: ["Spent a solid hour working on deploying a react app with github pages. Still don’t have it figured out yet. Hoping for some better luck tomorrow",
+            "Kept on it and got it working!"], 
+      id: 94
+    },
+    {
+      title: 'Day 95',
+      body: ["Watched youTube videos on react for multiple components, some css stuff and click events",
+            "Redoing the random quote generator project - still having issues fetching data from api and then displaying it in a component in an array without any errors",
+            "Much more comfortable now using github pages to deploy webpages though"], 
+      id: 95
+    },
+    {
+      title: 'Day 96',
+      body: ["Still working on the quote generator project with React. Figured out how to randomly render a quote from an api. Still need to figure out how to add a button that refreshes the page to get a new quote now",
+            "Watched a couple hours of a Udemy class on React that i've really been enjoying. helping me with many of the concepts and excited about that and to continue getting better with React"], 
+      id: 96
+    },
+    {
+      title: 'Day 97',
+      body: ["Continuing to learn React - working on useState hooks and pulling data objects from APIs"], 
+      id: 97
+    },
+    {
+      title: 'Day 98',
+      body: ["Continuing to learn React - you tube tutorials on state and props making reusable components"], 
+      id: 98
+    },
+    {
+      title: 'Day 99',
+      body: ["-Fiiiiiiiiiiinally figured out how to set an onclick handle to refresh data from an API and display it for the random quote generator project. Ahhh its been several days",
+            "Continuing with youTube react tutorial with videos on functions as props and useEffect"], 
+      id: 99
+    },
+    {
+      title: 'Day 100',
+      body: ["Day 100!!! Seems only fitting to end the last day of the challenge with the beginnings of a personal portfolio project documenting each day's progress. I used git hub pages to deploy the site. It only has the first 5 days so far but I will keep adding to it"], 
+      id: 100
+    },
+    {
+      title: 'Day 101',
+      body: ["Working through a React course on Udemy",
+            "reformatted my personal project of adding each days progress using a nested map function to display the info in a list format"], 
+      id: 101
+    },
+    {
+      title: 'Day 102',
+      body: ["Working through a React course on Udemy",
+            "added more days to my 100 days of coding project site and updated the readme file from default react to a custom welcome message"], 
+      id: 102
+    },
+    {
+      title: 'Day 103',
+      body: ["Working through a React course on Udemy",
+            "added the rest of the days to the 100 days of coding project site"], 
+      id: 103
+    },
   ]);
   
   const [name, setName] = useState('mario');
--- src/navbar.js
+++ src/navbar.js
@@ -2,10 +2,6 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Andrew Koller's 100 Days of Coding</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Article</a>
-            </div>
         </nav>
      );
 }
